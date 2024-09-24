9e26cd6f8b52b4874fb4fd5905c1fc50be445ecb
volagabriele <g.vola.3347@vallauri.edu>
2024-09-24 10:06:45 +0200

    Aggiungere i file di progetto.

---
+++ ES04_VolaPassaggioDatiProperty/frmPartenza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ES04_VolaPassaggioDatiProperty
+{
+    public partial class frmPartenza : Form
+    {
+        public frmPartenza()
+        {
+            InitializeComponent();
+        }
+    }
+}
